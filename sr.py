6b0d2f7d733275d07576f62d2f20ffb3697ac4b4
srchaos <172146899@qq.com>
2018-08-04 13:58:45 +0800

    latest update

--- sr.py
+++ sr.py
@@ -10,3 +10,9 @@ print(re.findall('[0-9]',s))
 #'^'可以匹配开头任意字符
 print(re.search('^aa','aaaycy'))
 #'$'匹配字符结尾
+#'*'匹配前面一个字符零次或多次
+print(re.search('aa*','aaa211sada'))
+#"+'匹配任意一个字符1次或多次
+#\A,\d,\D,\w,\W,\s
+ss='340803199401092877'
+print(re.search('(?P<province>[0-9]{4})(?P<city>[0-9]{2})(?P<born_year>[0-9]{8})',ss).groupdict())
